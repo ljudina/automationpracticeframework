765ed990e368b59e45e593ff40837988863c240f
Marko Jovanovic <ljudina@gmail.com>
2022-10-10 19:15:25 +0200

    Added TestConstants with username and password

---
+++ AutomationPracticeFramework/Helpers/TestConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationPracticeFramework.Helpers
+{
+    public static class TestConstants
+    {
+        public const string Username = "test+12345@test.com";
+        public const string Password = "QAKurs";
+    }
+}
